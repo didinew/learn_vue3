ea43d20103517689b8a4144056ebc3dd466e3c65
di <1426567242@qq.com>
2024-11-26 20:37:25 +0800

    elment

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-
+import Dialog from './components/Dialog.vue'
 </script>
 
 <template>
-  <div>
-
-    app
+  <div class="container">
+    我是父组件
+    <Dialog />
   </div>
 </template>
 
 <style lang="scss">
-
+.container {
+  width: 100%;
+  height: 800px;
+  background-color: yellow;
+}
 </style>
---
+++ src/components/Dialog.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="dialog">
+    <header class="header">
+      <div>
+        我是弹框
+      </div>
+      <el-coin>
+        <CloseBold />
+      </el-coin>
+    </header>
+    <main class="main">
+      我的内容是123456
+    </main>
+    <footer class="footer">
+      <el-button size="small">取消</el-button>
+      <el-button size="small" type="primary">确定</el-button>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+.dialog {
+    width: 400px;
+    height: 400px;
+    position: absolute;
+    top:    50%;
+    left:   50%;
+    background-color: #fff;
+    
+}
+</style>
